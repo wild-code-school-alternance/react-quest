eafc3cbb0b7da6c8e62131107294498c2d0d7a7b
dldvlpr <dormoy.dev@gmail.com>
2023-07-19 17:17:22 +0200

    add props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
 import PokemonCard from "./components/PokemonCard.jsx";
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function App() {
 
     return (
-        <PokemonCard/>
+        <PokemonCard pokemon = {pokemonList[0]}/>
     )
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,4 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
-function PokemonCard() {
-    const pokemon = pokemonList[0];
-    // const pokemon = pokemonList[1];
+function PokemonCard({pokemon}) {
     return (
         <figure>
             {pokemon.imgSrc ? (
