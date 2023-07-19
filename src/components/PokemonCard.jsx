1bdc8d4c3b3bcd7331d02f6698719cae73d3863c
dldvlpr <dormoy.dev@gmail.com>
2023-07-19 14:32:22 +0200

    First component with pokemon card

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+    return (
+        <figure>
+            <img
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                alt="My first Bulbasaur"/>
+            <figcaption>My first Bulbasaur</figcaption>
+        </figure>
+    )
+}
+
+export default PokemonCard;
