930a2f63dfc809fa5e63e6cb2974a190c2897116
dldvlpr <dormoy.dev@gmail.com>
2023-07-19 16:59:54 +0200

    finish quest 03 add ternaire and array

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,27 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+    // const pokemon = pokemonList[1];
     return (
         <figure>
-            <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="My first Bulbasaur"/>
-            <figcaption>My first Bulbasaur</figcaption>
+            {pokemon.imgSrc ? (
+                <img src={pokemon.imgSrc} alt={pokemon.name}/>
+            ) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
-    )
+    );
 }
 
 export default PokemonCard;
