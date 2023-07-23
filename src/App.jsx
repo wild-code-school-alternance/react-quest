6467be4674df96f5e97ac123f4c35a112ba4237f
dldvlpr <dormoy.dev@gmail.com>
2023-07-23 11:15:53 +0200

    add two condition for rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import PokemonCard from "./components/PokemonCard.jsx";
+import {useState} from "react";
 
 const pokemonList = [
     {
@@ -8,15 +9,54 @@ const pokemonList = [
             "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+        name: "charmander",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+        name: "squirtle",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+        name: "pikachu",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
         name: "mew",
     },
 ];
 
 function App() {
+    let [pokemonIndex, setPokemonIndex] = useState(0)
 
+    const handleClickPrevious = () => {
+        setPokemonIndex(pokemonIndex - 1)
+    }
+
+    const handleClickNext = () => {
+        setPokemonIndex(pokemonIndex + 1)
+    }
+
+    console.log(pokemonIndex)
     return (
-        <PokemonCard pokemon = {pokemonList[0]}/>
-    )
+        <div>
+            <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+            <div className="button-div">
+                {pokemonList.length > 1 && pokemonIndex !== 0 && (
+                    <button onClick={handleClickPrevious} className="pokemon-button-list">
+                        Previous
+                    </button>
+                )}
+                {pokemonList.length > 1 && pokemonIndex !== pokemonList.length - 1 && (
+                    <button onClick={handleClickNext} className="pokemon-button-list">
+                        Next
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App
